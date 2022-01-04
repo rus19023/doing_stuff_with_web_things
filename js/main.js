0b806bc446b3c9f5c4706b668530a33a6e6701e5
rus19023 <rus19023@byui.edu>
2022-01-03 19:43:48 -0500

    update week1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 // create link list element
-const list = getElementById("list");
+var list = document.getElementById("list");
+var ol = document.createElement("ol");
+var li = document.createElement("li");
+var li2 = document.createElement("li");
+var label, url;
 // create links array
-const links = [
-  {
-    label: "Week 01 notes",
-    week: $weekno
-  }
-];
+let weekno = 1;
+for (let i = 0; i <= weekno; i++) {
+  label = `Week ${i} Notes`;
+  url = `week${i}/`;
+  console.log(label);
+  console.log(url);
+  label2 = `Exercise ${i}`;
+  url2 = `exercises/exercise${i}.html`;
+  console.log(label2);
+  console.log(url2);
+  li.innerHTML = `<a href="${url}">${label}</a>`;
+  li2.innerHTML = `<a href="${url2}">${label2}</a>`;
+  ol.appendChild(li);
+  ol.appendChild(li2);
+  list.appendChild(ol);
+}
