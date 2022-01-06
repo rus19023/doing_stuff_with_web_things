2b2ffbd1182aa25b3afc8225b9c88989b2b91334
rus19023 <rus19023@byui.edu>
2022-01-06 11:57:13 -0500

    fix root url

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Week 01
+
 function fileExists(url)
 {
     var http = new XMLHttpRequest();
@@ -7,14 +9,22 @@ function fileExists(url)
 }
 
 // create link list element
-var container = document.getElementById("container");
-var ol = "<ol>";
-// create links list
+const container = document.getElementById("container");
+let ol = "<ol>";
+let termdate = new Date(2022, 0, 5, 0, 59, 0);
+let today = new Date();
+let seconds = Math.floor(today.getTime() - termdate.getTime() / (1000 * 60 * 60 * 24 * 7));
 let weekno = 1;
+let baseurl = "https://rus19023.github.io/myportfolio/";
+console.log(`termdate: ${termdate}`);
+console.log(`today: ${today}`);
+console.log(`seconds: ${seconds}`);
+console.log(`weekno: ${weekno}`);
+// create links list
 for (let i = 1; i <= weekno; i++) {
-  let n1 = `/myportfolio/week${i}/`;
-  let e1 = `/myportfolio/week${i}/exercise${i}.html`;
-  let c1 = `/myportfolio/week${i}/challenge${i}.html`;
+  let n1 = `${baseurl}week${i}/`;
+  let e1 = `${baseurl}exercises/exercise${i}.html`;
+  let c1 = `${baseurl}challenges/challenge${i}.html`;
   console.log(`n1: ${n1}`);
   console.log(`e1: ${e1}`);
   console.log(`c1: ${c1}`);
