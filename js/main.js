cd34865a42ce87e36cf33dbfa6f118313d492ac1
rus19023 <rus19023@byui.edu>
2022-01-08 15:00:02 -0500

    updated style, notes and exercises

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 // Week 01
 
 function loadStory() {
-  var storyName = document.getElementById("name_input").value
-  var storyHTML = localStorage.getItem(storyName)
-  document.getElementById("story_editor").value = storyHTML
+  let storyName = document.getElementById("name_input").value;
+  let storyHTML = localStorage.getItem(storyName);
+  document.getElementById("story_editor").value = storyHTML;
 }
 
 function saveStory() {
-  var storyName = document.getElementById("name_input").value
-  var storyHTML = document.getElementById("story_editor").value
-  localStorage.setItem(storyName,storyHTML)
+  let storyName = document.getElementById("name_input").value;
+  let storyHTML = document.getElementById("story_editor").value;
+  localStorage.setItem(storyName,storyHTML);
 }
 
 function showStory() {
-  var storyHTML = document.getElementById("story_editor").value
-  document.getElementById("story_show").innerHTML = storyHTML
+  let storyHTML = document.getElementById("story_editor").value;
+  document.getElementById("story_show").innerHTML = storyHTML;
 }
 
 function reverseNumber() {
@@ -22,5 +22,15 @@ function reverseNumber() {
   console.log(`number: ${number}`);
   let rnumber = "Number reversed: " + number.split("").reverse().join("");
   console.log(`rnumber: ${rnumber}`);
-  document.getElementById("reverse_output").innerHTML = "Number reversed: " + number.split("").reverse().join("");
+  document.getElementById("reverse_output").innerHTML = rnumber;
+}
+
+function loopingTriangle() {
+    var base = '#';
+    var triangle = '#';
+    for (let i = 0; i < 7; i++) {
+      base += '#';
+      triangle += '\n' + base;
+    }
+    document.getElementById("triangle_output").innerHTML = base;
 }
--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,26 @@
+const navlist = [
+    {
+        "name": "Week 01 Notes",
+        "url": "week01/"
+    },
+    {
+        "name": "Week 01 Exercises",
+        "url": "exercises/exercise1.html"
+    },
+    {
+        "name": "Week 02 Notes",
+        "url": "week02/"
+    },
+    {
+        "name": "Week 02 Exercises",
+        "url": "exercises/exercise2.html"
+    },
+    {
+        "name": "Week 02 Challenges",
+        "url": "challenges/challenge2.html"
+    }
+]
+
 function fileExists(url) {
   var http = new XMLHttpRequest();
   http.open("HEAD", url, false);
@@ -5,30 +28,36 @@ function fileExists(url) {
   return http.status != 404;
 }
 
+function weeknumber() {
+    // calculate which week we are on
+    // set first date of term
+    let termdate = new Date(2022, 0, 1, 0, 59, 0);
+    // set today's date
+    let today = new Date();
+    // calculate number of milliseconds per week
+    let secondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+    // calculate difference between today and term date in weeks
+    let seconds = today.getTime() - termdate.getTime();
+    // add 1 to weeks to account for rounding down
+    let weeknumber = Math.floor(seconds / secondsPerWeek) + 1;
+    console.log(`termdate: ${termdate}`);
+    console.log(`today: ${today}`);
+    console.log(`seconds: ${seconds}`);
+    console.log(`secondsPerWeek: ${secondsPerWeek}`);
+    console.log(`weeknumber: ${weeknumber}`);
+    // return week number
+    return weeknumber;
+}
+
 function createMenu() {
   // create link list container element
   const container = document.getElementById("container");
   // create link list element
   let ol = "<ol>";
-  // calculate which week we are on
-  // set first date of term
-  let termdate = new Date(2022, 0, 1, 0, 59, 0);
-  // set today's date
-  let today = new Date();
-  // calculate number of milliseconds per week
-  let secondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-  // calculate difference between today and term date in weeks
-  let seconds = today.getTime() - termdate.getTime();
-  // add 1 to weeks to account for rounding down
-  let weekno = Math.floor(seconds / secondsPerWeek) + 1;
   let baseurl = "https://rus19023.github.io/myportfolio/";
   //let baseurl = "/";
-  console.log(`termdate: ${termdate}`);
-  console.log(`today: ${today}`);
-  console.log(`seconds: ${seconds}`);
-  console.log(`secondsPerWeek: ${secondsPerWeek}`);
-  console.log(`weekno: ${weekno}`);
-  // get list of files to create links for each weekno
+  // get list of files to create links for each week number
+  let weekno = weeknumber();
   for (let i = 1; i <= weekno; i++) {
     //let folderString = `/${folder}${i}/${file}`;
     //let folderString = `${baseurl}${folder}${i}/${file}`;
@@ -51,5 +80,20 @@ function createMenu() {
   container.innerHTML = ol;
   console.log(`container: ${container.innerHTML}`);
 }
-createMenu();
 
+function createNav(array) {
+    // create link list container element
+    const container = document.getElementById("container");
+    // create link list element
+    let ol = "<ol>";
+    //let baseurl = "https://rus19023.github.io/myportfolio/";
+    let baseurl = "/";
+    // get list of files to create links for each week number
+    array.forEach(element => {
+        ol += `<li><a href="${baseurl}${element.url}">${element.name}</a></li>`;
+    });
+    ol += "</ol>";
+    container.innerHTML = ol;
+}
+createNav(navlist);
+//createMenu();
